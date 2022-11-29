5d7f42112da06146b3460b8ad52e1eaf2b715621
GANESHKUMAR <ganesh9ot@gmail.com>
2022-11-29 09:17:17 +0530

    # values

---
+++ values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+	name = "Go"
+	fmt.Println("value of name:", name)
+	var age int
+	age = 26
+	fmt.Println("value of age:", age)
+	var price float32
+	price = 26.57
+	fmt.Println("value of prince:", price)
+	var result bool
+	result = true
+	fmt.Println("values of result:", result)
+}
